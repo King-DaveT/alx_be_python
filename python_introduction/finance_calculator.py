1d7a1846652bd771e977c658499826cd570a0935
King-DaveT <davidak.tech@gmail.com>
2025-06-03 06:48:32 +0000

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,1 +1,11 @@
 
+# Define variables, x,y, m and ym.
+x = input("Enter your monthly income")
+y = input("Enter your total monthly expenses")
+a =int(x)
+b= int(y)
+m = a - b
+ym = (m *12) + (m*12*0.05)
+#Calculating savings and interest
+print( "Your monthly savings are", m)
+print("Projected savings after one year""," "with interest" "," "is", ym)
