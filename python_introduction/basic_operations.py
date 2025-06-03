612c43d5ed75a373df1fc754668f5aff363e38bf
King-DaveT <davidak.tech@gmail.com>
2025-06-03 00:05:49 +0000

    Update basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+# Define the variables and assign values
+number1 = 10
+number2 = 5
+
+# calculating the sum, difference and product
+sum = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+#Displaying the results
+print("sum": "
