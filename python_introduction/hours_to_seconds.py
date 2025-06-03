5f878ea1243522cc60e98166000deff878cfe06a
King-DaveT <davidak.tech@gmail.com>
2025-06-03 06:06:13 +0000

    Update hours_to_seconds.py

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,6 +1,6 @@
 # Define variables, hours and seconds
 hours = 2
-seconds = 3600
+seconds = 3600*hours
 
 #Converting hours to seconds
-print(f"{hours} hour(s) is {hours*seconds} seconds")
+print(f"{hours} hour(s) is {seconds} seconds")
