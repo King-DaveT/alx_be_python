f9b9488f33ddec9516a8811f084ca99f5d2be5d7
King-DaveT <davidak.tech@gmail.com>
2025-06-03 05:50:07 +0000

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,7 +2,7 @@
 principal = 1000
 rate = 0.05
 time = 3
+interest = (principal * rate * time)
 
 # Calculating the interest
-interest = principal * rate * time
 print(f"The simple interest is: {interest})
