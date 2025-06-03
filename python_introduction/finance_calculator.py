2047c8322eb77e60d7f33edd1f856f51fa6c4256
King-DaveT <davidak.tech@gmail.com>
2025-06-03 08:55:56 +0000

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,7 +3,7 @@ x = input("Enter your monthly income:")
 y = input("Enter your total monthly expenses:")
 monthly_income =float(x)
 monthly_expenses = float(y)
-monthly_savings = float(monthly_income - monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 #Converting hours to seconds
 print("Your monthly savings are", monthly_savings)
