2a92e1907b861eb1b1c7bdfa78f80e68b9c80d0d
King-DaveT <davidak.tech@gmail.com>
2025-06-03 06:33:00 +0000

    Update future_age_calculator.py

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,1 +1,8 @@
-
+# Define variables, age is in years
+x = input("How old are you?")
+y = 27
+a =int(x)
+b= int(y)
+age = a + b
+#Converting hours to seconds
+print("In 2050, you will be", age,"years old.")
