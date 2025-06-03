d372c7321f3913412be68b2be03cb2ebf954a679
King-DaveT <davidak.tech@gmail.com>
2025-06-03 05:09:29 +0000

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,7 @@
+# Define variables, principal is a currency($), rate is a percentage and time is in years
+principal = 1000
+rate = 0.05
+time = 3
+Interest = principal*rate*time
 
+print("The product of", principal, rate, "and", time, "is", interest)
