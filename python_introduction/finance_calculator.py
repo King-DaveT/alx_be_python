02255bd7f87d5d9e0b21988d0505e9031c68a47a
King-DaveT <davidak.tech@gmail.com>
2025-06-03 08:51:07 +0000

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,11 @@
 # Define variables, hours and seconds
 x = input("Enter your monthly income")
 y = input("Enter your total monthly expenses")
-monthly_income =int(x)
-monthly_expenses = int(y)
-monthly_savings = int(monthly_income - monthly_expenses)
+monthly_income =float(x)
+monthly_expenses = float(y)
+monthly_savings = float(monthly_income - monthly_expenses)
 projected_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 #Converting hours to seconds
-print( "Your monthly savings are", monthly_savings)
+print("Your monthly savings are", monthly_savings)
 print("Projected savings after one year""," "with interest" "," "is", projected_savings)
+
